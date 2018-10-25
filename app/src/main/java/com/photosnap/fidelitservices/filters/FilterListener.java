8160781276f99102ededf6816dc829db7c08f4d5
Ganesh Varade <ganesh.varahadek@gmail.com>
2018-10-25 12:17:59 +0530

    Adding all files

---
+++ app/src/main/java/com/photosnap/fidelitservices/filters/FilterListener.java
@@ -0,0 +1,7 @@
+package com.photosnap.fidelitservices.filters;
+
+import ja.photosnap.photoeditor.PhotoFilter;
+
+public interface FilterListener {
+    void onFilterSelected(PhotoFilter photoFilter);
+}
